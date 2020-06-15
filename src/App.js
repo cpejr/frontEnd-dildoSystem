a34cbb44739da872cfb6e6f2a03066b4894f871b
joao pedro <jpmm.brasil@hotmail.com>
2020-06-15 14:31:33 -0300

    arrumadinha

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import React from 'react';
 import './global.css';
 
 import Routes from './routes';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
 
 function App() {
+  const theme = createMuiTheme({
+    palette: { primary: { main: "#DAA621", contrastText: "#FFF" }}
+  });
   return (
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <Routes />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap" rel="stylesheet" />
+    </ ThemeProvider>
   );
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,7 @@
  import React, { useState } from 'react';
  import { Link, useHistory } from 'react-router-dom';
- import {} from 'react-icons/fi'; // importando o feather icons caso precise usar os icones do react
+ import { FiMail, FiLock } from 'react-icons/fi'; // importando o feather icons caso precise usar os icones do react
+ import { TextField, InputAdornment, Button } from '@material-ui/core'
 
 import api from '../../services/api';
 
@@ -8,15 +9,16 @@ import api from '../../services/api';
  import '../../global.css';
 
 function Login(){
-    const [id, setId] = useState('');
+    const [email, setEmail] = useState('');
     const [passwd, setPasswd] = useState('');
+    const [error, setError] = useState();
     const history = useHistory();
 
     async function handleLogin(e) {
         e.preventDefault();
 
         try{
-            const response = await api.post('login', { id }); //acho q tem q mudar id por accessToken
+            const response = await api.post('login', { email, password: passwd });
             
             localStorage.setItem('name', response.data.name);
             localStorage.setItem('accessToken', response.data.id);
@@ -24,7 +26,7 @@ function Login(){
             console.log(response.data.name);
             history.push("products");
         } catch (err) {
-            alert('Falha no Login, tente novamente!');
+            setError(err.response.data.message);
             console.log(err);
         }
     }
@@ -33,24 +35,46 @@ function Login(){
         <div className="login-container">
             <div className="content">
                 <section className="form">
-                    <form onSubmit={handleLogin}>
                     <h1>LOGIN</h1>
-                        <input
-                        placeholder="E-mail"
-                        value ={id}
-                        onChange={e => setId(e.target.value)}
-                        />
+                        <div className="line"/>
+                        <div className="container">
+                            <TextField
+                            className="input"
+                            InputProps={{
+                                startAdornment: (
+                                <InputAdornment position="start">
+                                    <FiMail size={20} />
+                                </InputAdornment>)
+                            }}
+                            placeholder="E-mail"
+                            variant="outlined"
+                            value={email}
+                            type="email"
+                            onChange={e => setEmail(e.target.value)}
+                            />
+                            <TextField
+                            className="input"
+                            InputProps={{
+                                startAdornment: (
+                                <InputAdornment position="start">
+                                    <FiLock size={20} />
+                                </InputAdornment>)
+                            }}
+                            variant="outlined"
+                            placeholder="Senha"
+                            value ={passwd}
+                            type="password"
+                            onChange={e => setPasswd(e.target.value)}
+                            />
 
-                        <input
-                        placeholder="Senha"
-                        value ={passwd}
-                        onChange={e => setPasswd(e.target.value)}
-                        />
-
-                        <button className="button" type="submit"> Entrar </button>
-                    </form>
-
-                    <Link className="button" to="/register"> Cadastrar </Link>
+                            {error && <p className="errortext">
+                                {error}    
+                            </p>}
+                            <Button className="button" type="submit" variant="contained" color="primary" onClick={handleLogin}> Entrar </Button>
+                        </div>
+                    <Link className="link" to="/register"> 
+                        <Button className="button" type="submit" color="primary" variant="outlined"> Cadastrar </Button>
+                    </Link>
                 </section>
             </div>
         </div>
