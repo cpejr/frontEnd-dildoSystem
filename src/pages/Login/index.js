03b7718c3cff1011dfa82ef34a53f4ac99a21712
Giovanna <giovanna.pimenta11@gmail.com>
2020-06-15 15:52:07 -0300

    comecei a estilizar o cadastro

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,14 @@
- import React, { useState } from 'react';
- import { Link, useHistory } from 'react-router-dom';
- import { FiMail, FiLock } from 'react-icons/fi'; // importando o feather icons caso precise usar os icones do react
- import { TextField, InputAdornment, Button } from '@material-ui/core'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiMail, FiLock } from 'react-icons/fi'; // importando o feather icons caso precise usar os icones do react
+import { TextField, InputAdornment, Button } from '@material-ui/core'
 
 import api from '../../services/api';
 
- import './styles.css';
- import '../../global.css';
+import './styles.css';
+import '../../global.css';
 
-function Login(){
+function Login() {
     const [email, setEmail] = useState('');
     const [passwd, setPasswd] = useState('');
     const [error, setError] = useState();
@@ -17,12 +17,12 @@ function Login(){
     async function handleLogin(e) {
         e.preventDefault();
 
-        try{
+        try {
             const response = await api.post('login', { email, password: passwd });
-            
+
             localStorage.setItem('name', response.data.name);
             localStorage.setItem('accessToken', response.data.id);
-            
+
             console.log(response.data.name);
             history.push("products");
         } catch (err) {
@@ -31,54 +31,54 @@ function Login(){
         }
     }
 
-    return(
+    return (
         <div className="login-container">
             <div className="content">
                 <section className="form">
                     <h1>LOGIN</h1>
-                        <div className="line"/>
-                        <div className="container">
-                            <TextField
+                    <div className="line" />
+                    <div className="container">
+                        <TextField
                             className="input"
                             InputProps={{
                                 startAdornment: (
-                                <InputAdornment position="start">
-                                    <FiMail size={20} />
-                                </InputAdornment>)
+                                    <InputAdornment position="start">
+                                        <FiMail size={20} />
+                                    </InputAdornment>)
                             }}
                             placeholder="E-mail"
                             variant="outlined"
                             value={email}
                             type="email"
                             onChange={e => setEmail(e.target.value)}
-                            />
-                            <TextField
+                        />
+                        <TextField
                             className="input"
                             InputProps={{
                                 startAdornment: (
-                                <InputAdornment position="start">
-                                    <FiLock size={20} />
-                                </InputAdornment>)
+                                    <InputAdornment position="start">
+                                        <FiLock size={20} />
+                                    </InputAdornment>)
                             }}
                             variant="outlined"
                             placeholder="Senha"
-                            value ={passwd}
+                            value={passwd}
                             type="password"
                             onChange={e => setPasswd(e.target.value)}
-                            />
+                        />
 
-                            {error && <p className="errortext">
-                                {error}    
-                            </p>}
-                            <Button className="button" type="submit" variant="contained" color="primary" onClick={handleLogin}> Entrar </Button>
-                        </div>
-                    <Link className="link" to="/register"> 
+                        {error && <p className="errortext">
+                            {error}
+                        </p>}
+                        <Button className="button" type="submit" variant="contained" color="primary" onClick={handleLogin}> Entrar </Button>
+                    </div>
+                    <Link className="link" to="/register">
                         <Button className="button" type="submit" color="primary" variant="outlined"> Cadastrar </Button>
                     </Link>
                 </section>
             </div>
         </div>
     );
- }
+}
 
- export default Login;
+export default Login;
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { TextField, Select, MenuItem, InputAdornment, Button } from '@material-ui/core';
 
 import api from '../../services/api';
 
- import './styles.css';
+import './styles.css';
 import { FiArrowLeft } from 'react-icons/fi';
 
- function Register(){
-     
+function Register() {
+
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -45,124 +46,150 @@ import { FiArrowLeft } from 'react-icons/fi';
             complement,
         };
 
-        try{
+        try {
             const response = await api.post('/register', data);
 
             alert(`Seu ID de acesso: ${response.data.id}`);
 
             history.push('/login');
-            
-        } catch (err){
+
+        } catch (err) {
             alert('Erro ao cadastrar usuario!');
         }
     }
-    
-    return(
+
+    return (
         <div className="register-container">
             <div className="content">
-                <h1> Cadastro </h1>
+                <h1>CADASTRO</h1>
+                <div className="line" />
 
                 <form onSubmit={handleRegister}>
-                        <input 
-                            placeholder="Nome"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                         />  
+                    <TextField
+                        variant="outlined"
+                        placeholder="Nome"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
 
-                        <input 
-                            placeholder="E-mail" 
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        
-                        <input
-                            placeholder="Senha" 
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        
-                        <input
-                            placeholder="Tipo"
-                            value={type}
-                            onChange={e => setType(e.target.value)} 
-                        />
-                        
-                        <input 
-                            placeholder="cpf"
-                            value={cpf}
-                            onChange={e => setCpf(e.target.value)}
-                        />
-                        
-                        <input 
-                            placeholder="Data de Aniversario" 
-                            value={birthdate}
-                            onChange={e => setBirthdate(e.target.value)}
-                        />
-                        
-                        <input 
-                            placeholder="CEP" 
-                            value={zipcode}
-                            onChange={e => setZipcode(e.target.value)}
-                        />
-                        
-                        <input 
-                            placeholder="Telefone" 
-                            value={phonenumber}
-                            onChange={e => setPhonenumber(e.target.value)}
-                        />                
-                   
+                    <TextField
+                        variant="outlined"
+                        placeholder="E-mail"
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <TextField
+                        variant="outlined"
+                        placeholder="Senha"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+
+                    <TextField
+                        id="select"
+                        label="Tipo"
+                        variant="outlined"
+                        value="20"
+                        value={type}
+                        onChange={e => setType(e.target.value)}
+                        select>
+                        <MenuItem value="10">Ten</MenuItem>
+                        <MenuItem value="20">Twenty</MenuItem>
+                    </TextField>
+
+                    <TextField
+                        variant="outlined"
+                        placeholder="CPF"
+                        type="number"
+                        value={cpf}
+                        onChange={e => setCpf(e.target.value)}
+                    />
+
+                    <TextField
+                        variant="outlined"
+                        type="date"
+                        placeholder="Data de Aniversario"
+                        value={birthdate}
+                        onChange={e => setBirthdate(e.target.value)}
+                    />
+
+                    <TextField
+                        variant="outlined"
+                        placeholder="CEP"
+                        value={zipcode}
+                        onChange={e => setZipcode(e.target.value)}
+                    />
+
+                    <TextField
+                        variant="outlined"
+                        placeholder="Telefone"
+                        type="tel"
+                        value={phonenumber}
+                        onChange={e => setPhonenumber(e.target.value)}
+                    />
+
                     <div className="input-group">
-                        <input 
-                            placeholder="UF" 
-                            style={{ width:80 }} 
+                        <TextField
+                            variant="outlined"
+                            placeholder="UF"
+                            style={{ width: 80 }}
                             value={state}
                             onChange={e => setState(e.target.value)}
                         />
-                        
-                        <input 
-                            placeholder="Cidade" 
+
+                        <TextField
+                            variant="outlined"
+                            placeholder="Cidade"
                             value={city}
                             onChange={e => setCity(e.target.value)}
                         />
                     </div>
                     <div className="input-group">
-                        <input 
-                            placeholder="Bairro" 
+                        <TextField
+                            variant="outlined"
+                            placeholder="Bairro"
                             value={neighborhood}
                             onChange={e => setNeighborhood(e.target.value)}
                         />
 
-                        <input 
-                            placeholder="Rua" 
+                        <TextField
+                            variant="outlined"
+                            placeholder="Rua"
                             value={street}
                             onChange={e => setStreet(e.target.value)}
                         />
                     </div>
                     <div className="input-group">
-                        <input 
-                            placeholder="Numero" 
+                        <TextField
+                            variant="outlined"
+                            placeholder="Numero"
                             value={number}
                             onChange={e => setNumber(e.target.value)}
                         />
-                    
-                        <input 
-                            placeholder="Complemento" 
+
+                        <TextField
+                            variant="outlined"
+                            placeholder="Complemento"
                             value={complement}
                             onChange={e => setComplement(e.target.value)}
                         />
                     </div>
 
-                    <button className="button" type="submit"> Cadastrar </button>
+                    <Button className="button" type="submit" variant="contained" color="primary" onClick={handleRegister}> Cadastrar </Button>
 
                 </form>
-                
+
                 <Link className="back-link" to="/login">
-                        <FiArrowLeft size={20} color="#a17402" />
+                    <FiArrowLeft size={20} color="#a17402" />
                          Login
                 </Link>
             </div>
         </div>
-     );
- }
+    );
+}
 
- export default Register;
+export default Register;
