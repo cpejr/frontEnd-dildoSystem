a9357f044912ead837cc7a3081e203c601673dc4
Vinicius Lara <viniciusfl1403@gmail.com>
2020-07-20 22:24:04 -0300

    Atualizando a página do carrinho
    Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { FiX, FiMinusCircle, FiPlusCircle } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 import { MdDeleteForever } from "react-icons/md";
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 import { Link } from "react-router-dom";
 
 import "./styles.css";
@@ -21,48 +23,51 @@ function Cart() {
               <FiX />
             </button>
           </h2>
-          <p>Entrega de x/xx/xxxx à x/xx/xxxx</p>
+          <p className="deliver-date">Entrega de x/xx/xxxx à x/xx/xxxx</p>
           <div className="cart-items">
             <div className="cart-card">
               <div className="cart-img">
                 <div className="empty"></div>
               </div>
-              <div>
-                <div>
-                  <h4>Gel de Massagem Corporal Lobisomem - 15 ml</h4>
-                </div>
-                <div>
-                  <p>Descrição um pouco mais detalhada do produto </p>
-                </div>
-                <div>
+              <div className="cardText">
+                <div className="info-text">
                   <div>
-                    <select name="size" id="size">
-                      <option value="unico">Tamanho único</option>
-                      <option value="P">P</option>
-                      <option value="M">M</option>
-                      <option value="G">G</option>
-                      <option value="GG">GG</option>
-                    </select>
+                    <h4>Gel de Massagem Corporal Lobisomem - 15 ml</h4>
                   </div>
+                  <div className="description">
+                    <p>Descrição um pouco mais detalhada do produto </p>
+                  </div>
+                  <div className="size-quantity">
+                    <div>
+                      <select name="size" id="size">
+                        <option value="unico">Tamanho único</option>
+                        <option value="P">P</option>
+                        <option value="M">M</option>
+                        <option value="G">G</option>
+                        <option value="GG">GG</option>
+                      </select>
+                    </div>
+                    <div className="itemQuantity">
+                      <RemoveCircleIcon className="less" size={25} />
+                      <p>1</p>
+                      <AddCircleIcon size={25} />
+                    </div>
+                  </div>
+                </div>
+                <div className="delete-price">
                   <div>
-                    <FiPlusCircle size={30} />
-                    <p>1</p>
-                    <FiMinusCircle size={30} />
+                    <MdDeleteForever className="delete" size={30} />
                   </div>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <MdDeleteForever size={35} />
-                </div>
-                <div className="full-price">
-                  <p>R$ 30,00</p>
-                </div>
-                <div className="discount-price">
-                  <p >R$ 25,00</p>
+                  <div className="full-price">
+                    <p>R$ 30,00</p>
+                  </div>
+                  <div className="discount-price">
+                    <p>R$ 25,00</p>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="borderEmpty"></div>
           </div>
           <div className="button-area">
             <button className="cart-primary-button">COMPRAR</button>
