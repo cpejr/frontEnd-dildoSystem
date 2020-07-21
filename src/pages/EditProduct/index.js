5aa76639fb273d31310803c1d4426326bf32cc4b
Vitor <vmarquesds@gmail.com>
2020-07-21 20:35:12 -0300

    Finalização do new Product

---
+++ src/pages/EditProduct/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NnEProduct from '../../components/NnEProduct/index.js'
+
+export default function EditProduct() {
+    return(
+        <div>
+            <NnEProduct />
+        </div>
+    )
+}
