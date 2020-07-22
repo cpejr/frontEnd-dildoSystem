d45b9bd0cd5ab28d3fc2c05a9b8bc1ca218d9646
Vinicius Lara <viniciusfl1403@gmail.com>
2020-07-22 00:36:27 -0300

    Header dashboard
    Modificações no header do dashboard

---
+++ src/components/Header/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import Logo from '../../images/CASULUS00LOGO.svg';
+import LogoName from '../../images/CASULUS01LOGONAME.svg';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
+import Burger from '../../components/Burger/index';
+
+import './index.css';
+
+export default function Header() {
+
+    return (
+        <div id="Header">
+            <div className="headerSuperior">
+                <div class="form-group has-search">
+                    <SearchIcon class="fa fa-search form-control-feedback searchIcon" />
+                    <input type="text" class="form-control searchInput" placeholder="Search" />
+                </div>
+                <ShoppingCartOutlinedIcon />
+                <PersonOutlinedIcon />
+            </div>
+            <div className="headerInferior">
+                <img className="headerImg" src={LogoName} alt="logo" />
+                <div className="links">
+                    <div className="emptyDiv"> </div>
+                    <div classNameName="empty" />
+                    <div className="dropdown">
+                        <button className="dropbtn">Cosméticos</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn">Acessórios</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn">Brincadeiras</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn">Próteses</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <img className="logoCasulusDashboard" src={Logo} alt="logo" />
+                    <div className="dropdown">
+                        <button className="dropbtn">Sado</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn">Moda Sensual</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn">Higiene e Banho</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn">Outros</button>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                </div>
+                <div classNameName="empty" />
+                <Burger />
+            </div>
+        </div>
+    );
+}
