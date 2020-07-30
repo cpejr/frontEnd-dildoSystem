8c2a1f2a5911d3ab24550d5a2a95d9eaab6470f4
Vinicius Lara <viniciusfl1403@gmail.com>
2020-07-30 10:26:28 -0300

    Pequena alteracao no headaer do dashboard
    Dropdown n estava funcionando e foi concertado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Logo from '../../images/CASULUS00LOGO.svg';
 import LogoName from '../../images/CASULUS01LOGONAME.svg';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import Burger from '../../components/Burger/index';
 
 import './index.css';
@@ -20,7 +21,7 @@ export default function Header() {
                     <SearchIcon class="fa fa-search form-control-feedback searchIcon" />
                     <input type="text" class="form-control searchInput" placeholder="Search" />
                 </div>
-                <div onClick={()=>{history.push('/cart')}}>
+                <div onClick={() => { history.push('/cart') }}>
                     <ShoppingCartOutlinedIcon />
                 </div>
                 <PersonOutlinedIcon />
@@ -31,68 +32,92 @@ export default function Header() {
                     <div className="emptyDiv"> </div>
                     <div classNameName="empty" />
                     <div className="dropdown">
-                        <button className="dropbtn">Cosméticos</button>
+                        <button className="dropbtn">Cosméticos <KeyboardArrowDownIcon /> </button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                            <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <button className="dropbtn">Acessórios</button>
+                        <button className="dropbtn">Acessórios <KeyboardArrowDownIcon /></button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <button className="dropbtn">Brincadeiras</button>
+                        <button className="dropbtn">Brincadeiras <KeyboardArrowDownIcon /></button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <button className="dropbtn">Próteses</button>
+                        <button className="dropbtn">Próteses <KeyboardArrowDownIcon /></button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                     <img className="logoCasulusDashboard" src={Logo} alt="logo" />
                     <div className="dropdown">
-                        <button className="dropbtn">Sado</button>
+                        <button className="dropbtn">Sado <KeyboardArrowDownIcon /></button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <button className="dropbtn">Moda Sensual</button>
+                        <button className="dropbtn">Moda Sensual <KeyboardArrowDownIcon /></button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <button className="dropbtn">Higiene e Banho</button>
+                        <button className="dropbtn">Higiene e Banho <KeyboardArrowDownIcon /></button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <button className="dropbtn">Outros</button>
+                        <button className="dropbtn">Outros <KeyboardArrowDownIcon /></button>
                         <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div className="emptyHeaderDiv"></div>
+                            <div className="dropdownLinks">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                 </div>
