65da42609c89aa813f12d718f97e1e55c00f0ee7
Matheus Novelli <matheus.mnovelli@gmail.com>
2020-08-02 17:17:18 -0300

    Criação Lista de Desejos

--- src/components/WishList/index.js
+++ src/components/WishList/index.js
@@ -13,16 +13,16 @@ export default function WishList() {
             <div className="wish-img"></div>
             <div className="wish-card-text">
               <h4>Álcool em Gel - Alcooss - 50ml</h4>
-              <p>R$15,00</p>
-              <strong>R$10,00</strong>
               <div>
-                <MdDeleteForever className="delete" size={30} /> 
+                <p>R$15,00</p>
+                <strong>R$10,00</strong>
               </div>
               <div className="wish-text"></div>
             </div>
-            <button className="wish-button">
-                Adicionar ao Carrinho
-            </button>
+            <div className="wish-button-area">
+              <MdDeleteForever className="wish-delete" size={30} />
+              <button className="wish-button">Adicionar ao Carrinho</button>
+            </div>
           </div>
         </div>
       </div>
