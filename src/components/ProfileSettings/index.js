4d6f7a8b070bf59e091aa443df108cec5564cbc9
Ian <ian.wanderleyx@gmail.com>
2020-09-13 16:35:38 -0300

    Página ''Meus Dados" agora puxa dados do usuário.

--- src/components/ProfileSettings/index.js
+++ src/components/ProfileSettings/index.js
@@ -1,8 +1,103 @@
 import React from "react";
 import "./styles.css";
 import { IoMdKey } from "react-icons/io";
+import api from "../../services/api";
+import {useEffect} from "react";
+import useStateWithPromise from "../../Contexts/useStateWithPromise";
 
-export default function ProfileSettings() {
+export default function ProfileSettings(props, { id, className, fileName, onSubmit }) {
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  //const [password, setPassword] = useState("");
+  //const [type, setType] = useState("");
+  const [cpf, setCpf] = React.useState(0);
+  //const [birthdate, setBirthdate] = useState(0);
+  const [zipcode, setZipcode] = React.useState(0);
+  const [phonenumber, setPhonenumber] = React.useState(0);
+  const [state, setState] = React.useState(""); 
+  const [city, setCity] = React.useState("");
+  const [neighborhood, setNeighborhood] = React.useState("");
+  const [street, setStreet] = React.useState("");
+  const [number, setNumber] = React.useState(0);
+  const [complement, setComplement] = React.useState("");
+
+  const LoginContext = React.createContext({});
+  const [accessToken, setAccessToken] = useStateWithPromise("");
+
+
+ /* async function handleSubmit(e) {
+    e.preventDefault();
+    
+    let data = new FormData();
+    function addToData(key, value) {
+      if (value !== undefined && value !== '')
+        data.append(key, value);
+    }
+
+    addToData('name', name);
+    addToData('email', email);
+    //addToData('password', password);
+    //addToData('type', type);
+    addToData('cpf', cpf);
+    //addToData('birthdate', birthdate);
+    addToData('zipcode', zipcode);
+    addToData('phonenumber', phonenumber);
+    addToData('state', state);
+    addToData('city', city);
+    addToData('neighrborhood', neighborhood);
+    addToData('street', street);
+    addToData('number', number);
+    addToData('complement', complement);
+
+    try {
+      const response = await api.get("user/:id", data, {
+          headers: {
+            "Content-Type" : "application/json",
+            authorization: "Bearer " + localStorage.accessToken,
+          }
+        })
+        alert(`Registro concluído!`, response);
+    } catch (err) {
+      console.log(JSON.stringify(err));
+      console.log(err.response);
+      alert("Register error");
+    }
+  } 
+   <form onSubmit={handleSubmit}> 
+   </form>
+  */
+
+  useEffect(() => {
+    const newToken = localStorage.getItem("accessToken");
+    if (newToken && !accessToken) {
+      async function grabData() {
+        const config = {
+          headers: { authorization: `Bearer ${newToken}` },
+        };
+        const resp = await api.get("verify", config);
+
+        if (resp.data.verified) {
+          await Promise.all([
+            setName(resp.data.user.name),
+            setEmail(resp.data.user.email),
+            setCpf(resp.data.user.cpf),
+            setPhonenumber(resp.data.user.phonenumber),
+            setState(resp.data.user.state),
+            setCity(resp.data.user.city),
+            setZipcode(resp.data.user.zipcode),
+            setNeighborhood(resp.data.user.neighborhood),
+            setStreet(resp.data.user.street),
+            setNumber(resp.data.user.number),
+            setComplement(resp.data.user.complement)
+          ]);
+        } 
+      }
+      grabData();
+    }
+  }, []);
+  
+
   return (
     <div className="settings-container">
       <h4>Meus Dados</h4>
@@ -12,19 +107,19 @@ export default function ProfileSettings() {
           <div className="settings-info">
             <div className="settings-info-item">
               <strong>Nome</strong>
-              <p>Alceu Carvalho Pinto</p>
+              <p>{name}</p>
             </div>
             <div className="settings-info-item">
               <strong>E-mail</strong>
-              <p>wojciech@yahoo.com</p>
+              <p>{email}</p>
             </div>
             <div className="settings-info-item">
               <strong>CPF</strong>
-              <p>193.032.322-86</p>
+              <p>{cpf}</p>
             </div>
             <div className="settings-info-item">
               <strong>Número de Telefone</strong>
-              <p>(31)99753-9427</p>
+              <p>{phonenumber}</p>
             </div>
           </div>
           <div className="settings-button-area">
@@ -38,35 +133,31 @@ export default function ProfileSettings() {
         <div className="settings-adress">  
           <div className="settings-adress-item">
             <strong>Estado</strong>
-            <p>MG</p>
+            <p>{state}</p>
           </div>
           <div className="settings-adress-item">
             <strong>Cidade</strong>
-            <p>Belo Horizonte</p>
+            <p>{city}</p>
           </div>
           <div className="settings-adress-item">
             <strong>CEP</strong>
-            <p>31888-999</p>
+            <p>{zipcode}</p>
           </div>
           <div className="settings-adress-item">
             <strong>Bairro</strong>
-            <p>Esperança</p>
+            <p>{neighborhood}</p>
           </div>
           <div className="settings-adress-item">
             <strong>Rua</strong>
-            <p>Rua Flor Branca</p>
+            <p>{street}</p>
           </div>
           <div className="settings-adress-item">
             <strong>Número</strong>
-            <p>175</p>
+            <p>{number}</p>
           </div>
           <div className="settings-adress-item">
             <strong>Complemento</strong>
-            <p>Apto 305</p>
-          </div>
-          <div className="settings-adress-item">
-            <strong>Referência</strong>
-            <p>Próximo ao Bar do Tadeu</p>
+            <p>{complement}</p>
           </div>
         </div>
         <div className="settings-button-edit-area">
