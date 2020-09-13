6d990342149c554f194d034f35b7a0b93b6e717d
João Prates <jvdprates@hotmail.com>
2020-09-12 22:31:18 -0300

    Componente de teste já consegue fazer upload de múltiplas imagens

--- src/components/MultipleUploader/index.js
+++ src/components/MultipleUploader/index.js
@@ -2,45 +2,65 @@ import React, { useState } from "react";
 import api from "../../services/api";
 import "./styles.css";
 
-export default function ImageUpload({ onChange }) {
-    const [images_ids, setImages] = useState(null);
+export default function ImageUpload({ onChange, onSubmit }) {
+    const [images, setImages] = useState(null);
     const [images_names, setImagesNames] = useState();
 
-    const changeHandler = (evt) => {
-        const files = evt.target.files;
+    function changeHandler(evt){
+        let files = evt.target.files;
 
-        console.log("Files: ", files);
+        files = [...files];
 
         let images = [];
         let imagesNames = [];
+
 
         files.forEach((file) => {
             images.push(file)
             imagesNames.push(file.name);
         })
 
-
         setImages(images);
         setImagesNames(imagesNames);
     };
 
-    const handleSubmit = (evt) =>{
-        evt.preventDefault();
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        let data = new FormData();
+        function addToData(key, value) {
+            if (value !== undefined && value !== '')
+                data.append(key, value);
+        }
+        images.forEach((image) =>{
+            addToData('imageFiles', image);
+        })
+
+        console.log("Req data: ", data);
+
+        try {
+            const response = await api.post("images", data)
+            alert(`Upload com sucesso!`, response);
+        } catch (err) {
+            console.log(err);
+            console.log(err.response);
+            alert("Upload falho");
+        }
     }
 
     return (
         <div>
             <div>
                 <h4>Imagens:</h4>
-                {images_names ? images_names.map((name)=>{
-                    return(
-                        <h5>{name}</h5>
+                {images_names ? images_names.map((name) => {
+                    return (
+                        <h5 key={name}>{name}</h5>
                     )
                 }) : <h5></h5>}
             </div>
             <form>
-                <input type="file" name="files" onChange={(e) =>{ changeHandler(e)}} multiple/>
-                <button type="submit" onClick={(e) => {handleSubmit(e)}}>Enviar</button>
+                <input type="file" name="files" onChange={(e) => { changeHandler(e) }} multiple />
+                <button type="submit" onClick={(e) => { handleSubmit(e) }}>Enviar</button>
             </form>
         </div>
     );
