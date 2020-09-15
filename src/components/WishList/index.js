460b447a92a5517f060d6a98cc583f0a5ff18424
Vitor <vmarquesds@gmail.com>
2020-09-15 18:42:21 -0300

    Finalizacao e alteracao da interacao com o Login

--- src/components/WishList/index.js
+++ src/components/WishList/index.js
@@ -1,18 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./styles.css";
+import ImageLoader from 'react-loading-image';
+import loading from '../../images/Loading.gif';
 
 import api from "../../services/api";
 
 import { MdDeleteForever } from "react-icons/md";
+import { LoginContext } from "../../Contexts/LoginContext";
 
-export default function WishList() {
+export default function WishList(props) {
   const [list, setList] = useState([]);
 
+  const userInfo = useContext(LoginContext);
+  console.log(userInfo);
+
   useEffect(() => {
-    api.get('userwishlist').then(response => {
-      setList(response.data);
-      console.log(response.data)
-    })
+    const newToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: { authorization: `Bearer ${newToken}` },
+      };
+      api.get(`userwishlist/${userInfo.id}`, config).then(response => {        
+        console.log(` essa eh a response${response.data}`)
+        setList(response.data)
+      })
+      
+    
   }, [])
 
 
@@ -21,15 +33,20 @@ export default function WishList() {
       <h4>Lista de Desejos</h4>
       {list.map(products => (
 
-        <div className="wish-container">
+        <div className="wish-container" key={products.id}>
           <div className="wish-data">
             <div className="wish-card">
-              <div className="wish-img"></div>
+              <div className="wish-img">
+              <ImageLoader
+                  src={`https://docs.google.com/uc?id=${products.image_id}`}
+                  loading={() => <img src={loading} alt="Loading..." />}
+                  error={() => <div>Error</div>} />
+              </div>
               <div className="wish-card-text">
-                <h4>{products.user_id}</h4>
+                <h4>{products.name}</h4>
                 <div>
-                  <p>R$15,00</p>
-                  <strong>R$10,00</strong>
+                  <p>{products.client_price}</p>
+                  <strong>{products.client_sale_price}</strong>
                 </div>
                 <div className="wish-text"></div>
               </div>
