3afd119f426f922bfe24cf45b68d2ece3b16d0e9
Vinicius Lara <viniciusfl1403@gmail.com>
2020-09-29 21:49:27 -0300

    Atualização da tela de cadastro de usuário

--- src/components/CartCard/index.js
+++ src/components/CartCard/index.js
@@ -3,30 +3,31 @@ import { MdDeleteForever } from "react-icons/md";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 import ImageLoader from 'react-loading-image';
+import loading from '../../images/Loading.gif';
+import { Link } from 'react-router-dom';
 
 import "./styles.css"
 import { Button } from "@material-ui/core";
 import cart from "../../services/cart"
 
 function PriceElement(props) {
-
-    const product = props.product;
+    const product = props.product.product;
 
     if (product.wholesaler_price) {
         if (product.on_sale_wholesaler) {
             return (
                 <div className="price-container">
-                    <p className="preco-card cortado">{`R$ ${Number(props.product.wholesaler_price).toFixed(2)}`}</p>
+                    <p className="preco-card cortado">{`R$ ${Number(product.wholesaler_price).toFixed(2)}`}</p>
 
                     <p className="preco-promocao">
-                        {`R$ ${Number(props.product.wholesaler_sale_price).toFixed(2)}`}
+                        {`R$ ${Number(product.wholesaler_sale_price).toFixed(2)}`}
                     </p>
                 </div>
             )
         } else {
             return (
                 <div className="price-container">
-                    <span className="preco-card">{`R$ ${Number(props.product.wholesaler_price).toFixed(2)}`}</span>
+                    <span className="preco-card">{`R$ ${Number(product.wholesaler_price).toFixed(2)}`}</span>
                 </div>)
 
         }
@@ -34,16 +35,16 @@ function PriceElement(props) {
         if (product.on_sale_client) {
             return (
                 <div className="price-container">
-                    <p className="preco-card cortado">{`R$ ${Number(props.product.client_price).toFixed(2)}`}</p>
+                    <p className="preco-card cortado">{`R$ ${Number(product.client_price).toFixed(2)}`}</p>
 
                     <p className="preco-promocao">
-                        {`R$ ${Number(props.product.client_sale_price).toFixed(2)}`}
+                        {`R$ ${Number(product.client_sale_price).toFixed(2)}`}
                     </p>
                 </div>
             )
         } else {
             return (
-                <span className="preco-card">{`R$ ${Number(props.product.client_price).toFixed(2)}`}</span>
+                <span className="preco-card">{`R$ ${Number(product.client_price).toFixed(2)}`}</span>
             )
         }
     }
@@ -51,14 +52,34 @@ function PriceElement(props) {
 
 
 function CartCard(props) {
-    const [productQuantity, setProductQuantity] = useState(1);
+    const [productQuantity, setProductQuantity] = useState(props.product.quantity);
+    const [isVisible, setIsVisible] = useState(true);
 
-    function sumQuantity() {
+
+
+
+    function sumQuantity(productId) {
         setProductQuantity(productQuantity + 1)
+        let cart = JSON.parse(localStorage.cart);
+        for (var i = 0; i < cart.length; i++) {
+            if (productId === cart[i].product.id) {  //look for match with name
+                cart[i].quantity += 1;  //add two
+                break;  //exit loop since you found the person
+            }
+        }
+        localStorage.setItem("cart", JSON.stringify(cart));
     }
-    function lessQuantity() {
+    function lessQuantity(productId) {
         if (productQuantity > 0) {
             setProductQuantity(productQuantity - 1)
+            let cart = JSON.parse(localStorage.cart);
+            for (var i = 0; i < cart.length; i++) {
+                if (productId === cart[i].product.id) {  //look for match with name
+                    cart[i].quantity -= 1;  //add two
+                    break;  //exit loop since you found the person
+                }
+            }
+            localStorage.setItem("cart", JSON.stringify(cart));
         }
     }
 
@@ -66,10 +87,12 @@ function CartCard(props) {
         <>
             <div className="cart-card">
                 <div className="cart-img">
-                    {/* <ImageLoader
-                        src={`https://docs.google.com/uc?id=${}`}
-                        loading={() => <img src={} alt="Loading..." />}
-                        error={() => <div>Error</div>} /> */}
+                    <Link to={`/product/${props.productId}`} className="image-text-container">
+                        <ImageLoader
+                            src={`https://docs.google.com/uc?id=${props.image_id}`}
+                            loading={() => <img src={loading} alt="Loading..." />}
+                            error={() => <div>Error</div>} />
+                    </Link>
                     {/* <div className="empty"></div> */}
                 </div>
                 <div className="cardText">
@@ -91,24 +114,18 @@ function CartCard(props) {
                                 </select>
                             </div> */}
                             <div className="itemQuantity">
-                                <Button onClick={() => { lessQuantity() }} ><RemoveCircleIcon className="less" size={25} /></Button>
+                                <Button onClick={() => { lessQuantity(props.productId) }} ><RemoveCircleIcon className="less" size={25} /></Button>
                                 <p>{productQuantity}</p>
-                                <Button onClick={() => { sumQuantity() }} ><AddCircleIcon size={25} /></Button>
+                                <Button onClick={() => { sumQuantity(props.productId) }} ><AddCircleIcon size={25} /></Button>
                             </div>
                         </div>
                     </div>
                     <div className="delete-price">
                         <div>
-                            <MdDeleteForever className="delete" size={30} onClick={(e) => cart.deleteItem(props.productId)} />
+                            <MdDeleteForever className="delete" size={30} onClick={(e) => { cart.deleteItem(props.productId); setIsVisible(false) }} />
                         </div>
 
-                        <PriceElement product={props} />
-                        {/* <div className="full-price">
-                            <p>R$ 30,00</p>
-                        </div>
-                        <div className="discount-price">
-                            <p>R$ 25,00</p>
-                        </div> */}
+                        <PriceElement product={props.product} />
                     </div>
                 </div>
             </div>
