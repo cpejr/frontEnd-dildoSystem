f7675fb4d8a4b510d8c64788b8c3cc9a377e5b8b
Matheus Novelli <matheus.mnovelli@gmail.com>
2020-10-26 17:16:16 -0300

    feat: Cria uma página com os produtos com pouco estoque

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -10,18 +10,18 @@ import "./styles.css";
 
 import NnEProduct from "../../components/NnEProduct";
 
-import ProductEditor from "../../components/ProductEditor"
+import ProductEditor from "../../components/ProductEditor";
 import EditProduct from "../../pages/EditProduct";
 import AdminDashboard2 from "../TestAdmin";
 import Orders from "../../components/Orders";
 import PendingUsers from "../../components/PendingUsers";
 import Products from "../../components/Products/index.js";
-import Carousel from "../../components/Carousel/index.js"
+import Carousel from "../../components/Carousel/index.js";
 
-import PendingOrders from "../../components/PendingOrders/index.js"
+import PendingOrders from "../../components/PendingOrders/index.js";
 
-import AllUsers from "../../components/AllUsers"
-
+import AllUsers from "../../components/AllUsers";
+import LowStock from "../LowStock";
 
 function Admin(props) {
   let [nome, setNome] = useState("Nome do usuario");
@@ -31,64 +31,63 @@ function Admin(props) {
 
   return (
     <LoginContext.Consumer>
-      {
-        value => {
-          if (value.type === 'admin') {
-            return (
-              <div>
-                <AdminDashboard2 name={value.name} type={value.type}>
+      {(value) => {
+        if (value.type === "admin") {
+          return (
+            <div>
+              <AdminDashboard2 name={value.name} type={value.type}>
+                <Route exact path={props.match.path} component={Main} />
 
-                  <Route exact path={props.match.path} component={Main} />
+                <Route
+                  path={`${props.match.path}/newproduct`}
+                  component={NnEProduct}
+                />
 
-                  <Route
-                    path={`${props.match.path}/newproduct`}
-                    component={NnEProduct}
-                  />
+                <Route
+                  path={`${props.match.path}/productedit/:id`}
+                  component={ProductEditor}
+                />
 
-                   <Route
-                    path={`${props.match.path}/productedit/:id`}
-                    component={ProductEditor}
-                  />
+                <Route
+                  path={`${props.match.path}/editproduct`}
+                  component={EditProduct}
+                />
 
-                   <Route
-                    path={`${props.match.path}/editproduct`}
-                    component={EditProduct}
-                  /> 
+                <Route
+                  path={`${props.match.path}/pendingorder/:id`}
+                  component={Orders}
+                />
 
-                  <Route
-                    path={`${props.match.path}/pendingorder/:id`}
-                    component={Orders}
-                  />
+                <Route
+                  path={`${props.match.path}/allusers`}
+                  component={AllUsers}
+                />
 
-                  <Route
-                    path={`${props.match.path}/allusers`}
-                    component={AllUsers}
-                  />
+                <Route
+                  path={`${props.match.path}/pendingusers`}
+                  component={PendingUsers}
+                />
 
+                <Route
+                  path={`${props.match.path}/lowStock`}
+                  component={LowStock}
+                />
 
-                  <Route
-                    path={`${props.match.path}/pendingusers`}
-                    component={PendingUsers}
-                  />
-                      
                 <Route
                   path={`${props.match.path}/carousel`}
                   component={Carousel}
                 />
                 <Route
-                    path={`${props.match.path}/pendingorders`}
-                    component={PendingOrders}
-                  />
-                </AdminDashboard2>
-
-              </div>
-            );
-          } else {
-            history.push('/');
-          }
-
+                  path={`${props.match.path}/pendingorders`}
+                  component={PendingOrders}
+                />
+              </AdminDashboard2>
+            </div>
+          );
+        } else {
+          history.push("/");
         }
-      }
+      }}
     </LoginContext.Consumer>
   );
 }
