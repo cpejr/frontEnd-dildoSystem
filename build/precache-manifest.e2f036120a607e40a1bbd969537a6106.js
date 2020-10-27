0b23d0971dfe21a186f0f08b2b87c03daa30c7c2
João Prates <jvdprates@hotmail.com>
2020-10-27 10:50:29 -0300

    feat: Gerando build de produção dentro do projeto
    Para rodar a build de produção use o comando node main.js dentro da pasta build.

---
+++ build/precache-manifest.e2f036120a607e40a1bbd969537a6106.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecc20bfe16894405f94513768de78bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6d225effb9c0f14937",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d74d1ed11efc7018cbe1",
+    "url": "/static/css/main.b1c4a6e5.chunk.css"
+  },
+  {
+    "revision": "cd6d225effb9c0f14937",
+    "url": "/static/js/2.ed964ca4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.ed964ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74d1ed11efc7018cbe1",
+    "url": "/static/js/main.445b7057.chunk.js"
+  },
+  {
+    "revision": "6341619421c40f37f3b1",
+    "url": "/static/js/runtime-main.b03a7968.js"
+  },
+  {
+    "revision": "5a2d7525d94e266564b93c6193112082",
+    "url": "/static/media/CASULUS00LOGO.5a2d7525.svg"
+  },
+  {
+    "revision": "da6494a75467c7d1a636ed48545323e3",
+    "url": "/static/media/CASULUS01LOGODESIGN.da6494a7.svg"
+  },
+  {
+    "revision": "e0457a0e2362b44049161bb75dc7062d",
+    "url": "/static/media/CASULUS01LOGONAME.e0457a0e.svg"
+  },
+  {
+    "revision": "2437245d182f3f51cdec1863cae620e4",
+    "url": "/static/media/CASULUS02.2437245d.svg"
+  }
+]);
