445fad8af2c795828e9ec10b7f82a3ef599580a7
Elias <eliasfaria@ufmg.br>
2020-10-30 09:59:06 -0300

    Feat: deixando a edição de posição das imagens do carrossel funcional

--- src/components/Carousel/CarouselImages.js
+++ src/components/Carousel/CarouselImages.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import ImageLoader from "react-loading-image";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
@@ -6,23 +6,28 @@ import loading from "../../images/Loading.gif";
 import api from '../../services/api';
 
 function CarouselImages(props) {
-  const [newPosition, setNewPosition] = useState(props.image.position);
+  const [newPosition, setNewPosition] = useState();
+
+  useEffect(()=>{
+    if(!isNaN(props.image.position))
+    setNewPosition(props.image.position)
+  },[props.image.position]);
 
   const accessToken = localStorage.getItem('accessToken')
 
   const config = {
-      headers: { 'authorization': `Bearer ${accessToken}` },
+    headers: { 'authorization': `Bearer ${accessToken}` },
   }
 
   async function handleDelete(e) {
 
     try {
-      
+
       const response = await api.delete('/Carousel/' + props.image.id, config);
 
       alert(`Imagem deletada com sucesso!`);
       props.setUpdate(!props.update);
-  
+
 
     } catch (err) {
       console.log(err);
@@ -50,7 +55,9 @@ function CarouselImages(props) {
           <input
             type="text"
             value={newPosition}
-            onChange={(e) => setNewPosition(e.target.value)}
+            onChange={(e) => {setNewPosition(e.target.value)
+              props.handlePositionChange(props.image.id, e.target.value)          
+            }}
             className="form-control"
             id="validationDefaultUsername"
             aria-describedby="inputGroupPrepend2"
@@ -59,13 +66,13 @@ function CarouselImages(props) {
         </div>
       </div>
 
-                    <div className="enviar-button-carousel">
+      <div className="enviar-button-carousel">
 
-<button className="edit-delete" type="submit" onClick={handleDelete}>
-  Excluir Imagem
+        <button className="edit-delete" type="submit" onClick={handleDelete}>
+          Excluir Imagem
   <DeleteForeverIcon />
-</button>
-</div>
+        </button>
+      </div>
     </div>
   );
 }
