2ab59f17131353ddb5821336733cfc4d8dfc66e2
Leandro Gripp <lggcontas@gmail.com>
2020-11-18 12:58:00 -0300

    Feat: Nova build para ser mandada para a produção

---
+++ build/precache-manifest.6aaa3b97d1f87ae4e2ddebec86836429.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1460ebd496b4d322f6e2eca2e77e1685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91b28bd1b7a4429f2dee",
+    "url": "/static/css/2.beeb301c.chunk.css"
+  },
+  {
+    "revision": "b85d766e1560a3a19fcc",
+    "url": "/static/css/main.caf591e0.chunk.css"
+  },
+  {
+    "revision": "91b28bd1b7a4429f2dee",
+    "url": "/static/js/2.9e09546f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.9e09546f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b85d766e1560a3a19fcc",
+    "url": "/static/js/main.ca2ec039.chunk.js"
+  },
+  {
+    "revision": "6341619421c40f37f3b1",
+    "url": "/static/js/runtime-main.b03a7968.js"
+  },
+  {
+    "revision": "da6494a75467c7d1a636ed48545323e3",
+    "url": "/static/media/CASULUS01LOGODESIGN.da6494a7.svg"
+  },
+  {
+    "revision": "e0457a0e2362b44049161bb75dc7062d",
+    "url": "/static/media/CASULUS01LOGONAME.e0457a0e.svg"
+  },
+  {
+    "revision": "2437245d182f3f51cdec1863cae620e4",
+    "url": "/static/media/CASULUS02.2437245d.svg"
+  }
+]);
