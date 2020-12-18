e587431f40c317c1257c29ad3d45b19f6aef43eb
Elias <eliasfaria@ufmg.br>
2020-12-17 22:11:35 -0300

    Feat: implementando novo carrossel

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./styles.css";
 import api from "../../services/api";
+import nextIcon from "../../images/nextIcon.png";
+import prevIcon from "../../images/prevIcon.png";
 
 export default function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -25,17 +27,45 @@ export default function ControlledCarousel() {
 
   return (
     <div className="Carousel">
-      <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel activeIndex={index} onSelect={handleSelect} nextIcon={<span aria-hidden="true" className="" > <img src={nextIcon}/></span>} prevIcon={<span aria-hidden="true" className="" ><img src={prevIcon}/></span>}>
         {images.map((image, index) => {
+          let prev, next;
+          if (index == 0) {
+            prev = images.length - 1
+            next = index + 1
+            console.log(images[prev]);
+          } else if (index == images.length - 1) {
+            prev = index - 1;
+            next = 0
+          } else {
+            prev = index - 1;
+            next = index + 1;
+          };
           return (
             <Carousel.Item>
-              <a href={image.link}>
-                <img
-                  className="d-block w-100"
-                  src={`https://docs.google.com/uc?id=${image.image_id}`}
-                  alt={image}
-                />
-              </a>
+              <div className="CarouselImages">
+                <div className="previmage">
+                  <img
+
+                    src={`https://docs.google.com/uc?id=${images[prev].image_id}`}
+
+                  />
+                </div>
+                <a href={image.link}>
+                  <img
+                    className="d-block w-100"
+                    src={`https://docs.google.com/uc?id=${image.image_id}`}
+                    alt={image}
+                  />
+                </a>
+                <div className="nextimage">
+                  <img
+
+                    src={`https://docs.google.com/uc?id=${images[next].image_id}`}
+
+                  />
+                </div>
+              </div>
             </Carousel.Item>
           );
         })}
