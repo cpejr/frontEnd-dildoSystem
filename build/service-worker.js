2382487f6aa485cdc4969e2681ae0678bc4ff7fd
Leandro <lggcontas@gmail.com>
2021-01-25 10:11:31 -0300

    BREAKING CHANGE: Nova build, potencialmente final

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c1176a2355d99c11ab7a911b78e8757.js"
+  "/precache-manifest.60dd229b26a511df0eb50a3e38956a04.js"
 );
 
 self.addEventListener('message', (event) => {
