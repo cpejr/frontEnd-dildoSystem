d3907d63af38d1a4f037d8ba599cee3e8bfae79d
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-12-04 14:44:46 -0300

    fix: carousel bug

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -28,50 +28,49 @@ export default function ControlledCarousel() {
   };
 
   return (
-    <div className="Carousel">
-      <Carousel activeIndex={index} onSelect={handleSelect} nextIcon={<span aria-hidden="true" className="" > <img src={nextIcon}/></span>} prevIcon={<span aria-hidden="true" className="" ><img src={prevIcon}/></span>}>
-        {images.map((image, index) => {
-          let prev, next;
-          if (index == 0) {
-            prev = images.length - 1
-            next = index + 1
-            // console.log(images[prev]);
-          } else if (index == images.length - 1) {
-            prev = index - 1;
-            next = 0
-          } else {
-            prev = index - 1;
-            next = index + 1;
-          };
-          return (
-            <Carousel.Item key={index}>
-              <div className="CarouselImages">
-                <div className="previmage">
-                  <img
-
-                    src={`${urlAWS}/${images[prev].image_id}`}
-
-                  />
-                </div>
-                <a href={image.link}>
-                  <img
-                    className="d-block w-100"
-                    src={`${urlAWS}/${image.image_id}`}
-                    alt={image}
-                  />
-                </a>
-                <div className="nextimage">
-                  <img
-
-                    src={`${urlAWS}/${images[next].image_id}`}
-
-                  />
-                </div>
-              </div>
-            </Carousel.Item>
-          );
-        })}
-      </Carousel>
-    </div>
+    <>
+      {images && (
+        <div className="Carousel">
+          <Carousel activeIndex={index} onSelect={handleSelect} nextIcon={<span aria-hidden="true" className="" > <img src={nextIcon} /></span>} prevIcon={<span aria-hidden="true" className="" ><img src={prevIcon} /></span>}>
+            {images.map((image, index) => {
+              let prev, next;
+              if (index == 0) {
+                prev = images.length - 1
+                next = index + 1
+              } else if (index == images.length - 1) {
+                prev = index - 1;
+                next = 0
+              } else {
+                prev = index - 1;
+                next = index + 1;
+              };
+              return (
+                <Carousel.Item key={index}>
+                  <div className="CarouselImages">
+                    <div className="previmage">
+                      <img
+                        src={`${urlAWS}/${images[prev]?.image_id}`}
+                      />
+                    </div>
+                    <a href={image?.link}>
+                      <img
+                        className="d-block w-100"
+                        src={`${urlAWS}/${image?.image_id}`}
+                        alt={image}
+                      />
+                    </a>
+                    <div className="nextimage">
+                      <img
+                        src={`${urlAWS}/${images[next]?.image_id}`}
+                      />
+                    </div>
+                  </div>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </div>
+      )}
+    </>
   );
 }
